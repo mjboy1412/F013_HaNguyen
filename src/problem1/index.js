5e0904f73f2dfca3c07932ca21aa579e98b830e3
hanguyen <mjboy1412@gmail.com>
2023-10-03 14:41:21 +0700

    implementing problem1

---
+++ src/problem1/index.js
@@ -0,0 +1,20 @@
+export const sum_to_n_a = (n) => {
+	if (n <= 0) return 0;
+	return n + sum_to_n_a(n - 1);
+};
+
+export const sum_to_n_b = (n) => {
+	if (n <= 0) return 0;
+	return [...Array(n + 1).keys()].reduce((accumulator, currentValue) =>
+		accumulator + currentValue
+	)
+};
+
+export const sum_to_n_c = (n) => {
+	if (n <= 0) return 0;
+	let result = 0;
+	for (let i = 1; i <= n; i++) {
+		result = result + i;
+	}
+	return result;
+};
